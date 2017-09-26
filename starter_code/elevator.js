d5618ade00874862acf74a50bbb9233048f673f4
Liudmyla Iefremova <liudmyla.iefremova@hotmail.com>
2017-09-26 09:00:57 +0200

    sketch of 1 and 2 iteration

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,17 +3,41 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.direction = "up";///????
+    this.waitingList = [];
+    this.passengers = [];
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() {
+    let interval = setInterval(update, 1000);
+  }
+  stop() {
+    clearInterval(interval);
+  }
+  update() {
+    this.log();
+  }
   _passengersEnter() { }
   _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  floorUp() { () => {
+      this.floor < this.MAXFLOOR ? this.floor++ : this.MAXFLOOR;
+      this.direction = "up";
+    }
+  }
+  floorDown() { () => {
+      this.floor===0 ? 0: this.floor--;
+      this.direction ="down";
+    }
+  }
+  call(person) {
+    this.requests.pop(person);
+    this.waitingList.pop(person);
+  }
+  log() {
+    console.log(`Direction: ${this.direction==="up" ? "up" : "down"}
+                  | Floor: ${this.floor}`);
+   }
+
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,14 @@
+//'esversion: 6';
+
 const Elevator = require('./elevator.js');
+
+let elevator = new Elevator();
+
+elevator.log();
+
+
+// elevator.floorUp();
+// elevator.floorUp();
+elevator.floorDown();
+
+elevator.log();
