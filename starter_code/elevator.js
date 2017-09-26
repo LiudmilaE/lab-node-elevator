8e8685a15e00d090fbbd8c1b0f0baa53cad43dea
Liudmyla Iefremova <liudmyla.iefremova@hotmail.com>
2017-09-26 09:20:41 +0200

    edit few methods

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -6,6 +6,7 @@ class Elevator {
     this.direction = "up";///????
     this.waitingList = [];
     this.passengers = [];
+    this.listOfFloors = [];
   }
 
   start() {
@@ -14,11 +15,24 @@ class Elevator {
   stop() {
     clearInterval(interval);
   }
+
+  //In later iterations, the elevator will process the list of requests
+  // as it travels up and down.
+  // We will need a list of floors that the elevator should serve in the update method.
   update() {
     this.log();
   }
-  _passengersEnter() { }
-  _passengersLeave() { }
+  _passengersEnter(person) {
+    this.passengers.push(person);
+    console.log(`${person.name} has enter the elevator`);
+   }
+  _passengersLeave() {
+    this.passengers=this.passengers.filter(person => {
+      person.destinationFloor!==currFloor;
+      console.log(`${person.name} has left the elevator`);
+    });//how to remove ???
+
+   }
   floorUp() { () => {
       this.floor < this.MAXFLOOR ? this.floor++ : this.MAXFLOOR;
       this.direction = "up";
@@ -30,12 +44,11 @@ class Elevator {
     }
   }
   call(person) {
-    this.requests.pop(person);
-    this.waitingList.pop(person);
+    this.requests.push(person);
+    this.waitingList.push(person);
   }
   log() {
-    console.log(`Direction: ${this.direction==="up" ? "up" : "down"}
-                  | Floor: ${this.floor}`);
+    console.log(`Direction: ${this.direction==="up" ? "up" : "down"} | Floor: ${this.floor}`);
    }
 
 }
